c082b9201ab50c4ecac1c8427849ef4d6f045e45
Erwann Hilley <58454685+hphilley15@users.noreply.github.com>
2023-09-23 03:33:38 +0000

    conditional

--- src/magic_trick.py
+++ src/magic_trick.py
@@ -37,11 +37,35 @@ def run_test( shuffle, N = 6, N_TESTS = 1000 ):
             counts[i][v] = counts[i][v] + 1
     return pd.DataFrame( counts )
 
-fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
+def run_conditional_test( shuffle, N = 6, N_TESTS = 10000 ):
+    conditional_counts = np.zeros( ( N,N,N ) )
+    for t in range(N_TESTS):
+        arr = [ i for i in range( N) ]
+        out = shuffle( arr )
+        cond_ind = out[0]
 
-df = run_test( die_shuffle, N = 6, N_TESTS = 1000 )
+        for i,v in enumerate( out ):
+            conditional_counts[cond_ind,i,v] = conditional_counts[cond_ind, i, v ] + 1
+    return [ pd.DataFrame( conditional_counts[i] ) for i in range(len(conditional_counts ) ) ]
 
-df.plot(kind='bar', stacked=True, ax=ax1 )
 
-fig.savefig('./figures/die_shuffle.svg')
+
+# fig = plt.figure()
+# ax1 = fig.add_subplot(1,1,1)
+
+# df = run_test( die_shuffle, N = 6, N_TESTS = 1000 )
+
+# df.plot(kind='bar', stacked=True, ax=ax1 )
+
+# fig.savefig('./figures/die_shuffle.svg')
+
+dframes = run_conditional_test( die_shuffle, N = 6, N_TESTS = 10000 )
+
+for i in range(6):
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1,1,1)
+
+    dframes[i].plot(kind='bar', stacked=True, ax=ax1 )
+
+    fig.savefig(f'./figures/conditional_{i:03d}_die_shuffle.svg')
